528bccc382c80aa80d477a577217a721631ca072
tlooi <t.looi@outlook.com>
2022-12-04 23:16:32 +1300

    day 4

---
+++ day4/p1.ts
@@ -0,0 +1,15 @@
+import input from './input';
+
+const assignmentRanges = (input.split('\n') as string[]).map(val => val.split(',').map(val => val.split('-').map(val => parseInt(val))));
+
+let result = 0;
+
+assignmentRanges.forEach((ranges) => {
+    if (ranges[0][0] >= ranges[1][0] && ranges[0][1] <= ranges[1][1]) {
+        result++;
+    } else if (ranges[1][0] >= ranges[0][0] && ranges[1][1] <= ranges[0][1]) {
+        result++;
+    }
+});
+
+console.log(result);
